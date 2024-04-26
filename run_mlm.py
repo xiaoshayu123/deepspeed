5e8cfe882a806db683dfd54e97ed2ced324caa6f
xiaoshayu123 <1780399060@qq.com>
2024-04-27 00:46:40 +0800

    1.修改参数

--- run_mlm.py
+++ run_mlm.py
@@ -211,7 +211,8 @@ class SaveMetricsCallback(TrainerCallback):
         self.metrics_dataframe.to_excel(self.excel_filename,engine='xlsxwriter')
 
         fig, axs = plt.subplots(2)
-
+        print("输出指标")
+        print(self.metrics_dataframe.columns)
         # Plot accuracy
         axs[0].plot(self.metrics_dataframe['epoch'], self.metrics_dataframe['eval_accuracy'])
         axs[0].set(xlabel='Epoch', ylabel='Accuracy',
