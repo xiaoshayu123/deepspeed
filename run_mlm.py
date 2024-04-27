314d9f200d1db27d3cb4bde86a54ccdd369fc3c7
xiaoshayu123 <1780399060@qq.com>
2024-04-27 15:49:08 +0800

    1.修改参数

--- run_mlm.py
+++ run_mlm.py
@@ -206,7 +206,7 @@ class SaveMetricsCallback(TrainerCallback):
         print(f'epochEndState:{state}')
         print(f'epochEndEpoch {state.epoch} has ended.')
         print(f'epochEndlog_history {state.log_history} has ended.')
-        logs.perplexity = math.exp(logs["eval_loss"])
+        logs["perplexity"] = math.exp(logs["eval_loss"])
         self.metrics.append(logs)
         print(f'logEnd:{logs}')
 
