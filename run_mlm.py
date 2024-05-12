2b18066a7ff8b29798dc4e2517dab24bae0c28d7
@999dly <3431005337@qq.com>
2024-05-13 00:44:08 +0800

    1，增加准确率

--- run_mlm.py
+++ run_mlm.py
@@ -462,7 +462,6 @@ def main():
     trainer = Trainer(
         model=model,
         args=training_args,
-        compute_metrics=compute_metrics,
         train_dataset=train_dataset if training_args.do_train else None,
         eval_dataset=eval_dataset if training_args.do_eval else None,
         tokenizer=tokenizer,
