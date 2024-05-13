a0a804f711fcd629fd06518db2fac6a86b1c5243
@999dly <3431005337@qq.com>
2024-05-14 00:52:44 +0800

    1.增加训练代码

--- run_mlm.py
+++ run_mlm.py
@@ -173,7 +173,7 @@ class SaveMetricsCallback(TrainerCallback):
         print("Evaluating" , logs)
         item = state.log_history[-1]
         item["perplexity"] = math.exp(item["eval_loss"])
-        item["accuracy"] = logs["eval_accuracy"]  # 获取精确度
+        item["accuracy"] = item["eval_accuracy"]  # 获取精确度
         self.metrics.append(item)
 
     # 在训练结束后执行
