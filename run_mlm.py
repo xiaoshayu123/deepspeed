262289a5e609f4b0cded100417f6b141d04a14f8
@999dly <3431005337@qq.com>
2024-05-13 23:15:58 +0800

    1.增加训练代码

--- run_mlm.py
+++ run_mlm.py
@@ -464,6 +464,7 @@ def main():
         args=training_args,
         train_dataset=train_dataset if training_args.do_train else None,
         eval_dataset=eval_dataset if training_args.do_eval else None,
+        compute_metrics=compute_metrics,
         tokenizer=tokenizer,
         data_collator=data_collator,
         callbacks=[SaveMetricsCallback(data_args, data_args.out_excel, data_args.out_pic)]
